f5d4d370b4ef67171520c31b7f60ceb220ae6076
NPDebs <deborahudoh02@gmail.com>
2024-01-06 21:52:02 +0100

    Write function to convert currency
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- xchange.py
+++ xchange.py
@@ -9,3 +9,15 @@ def fetch_rates(key):
     return response.json()["rates"] if response.status_code == 200 else print("Couldn't fetch exchange rates. Status code:", response.status_code) 
     return None
 
+exchange_rates = fetch_rates(key)
+
+# function to convert currency
+def convert_currency(amount, from_currency, to_currency, exchange_rates):
+    if from_currency and to_currency in exchange_rates:
+        rate = exchange_rates[to_currency] / exchange_rates[from_currency]
+        converted_amount = amount * rate
+        return converted_amount
+    else:
+        print("Sorry. Your currency codes are invalid.")
+        return None
+
